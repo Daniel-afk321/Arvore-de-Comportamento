2ed2b811661d945f1559d41eb327c494b9094ea5
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2023-06-14 00:47:09 -0300

    Etapa1

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    //Velocidade de movimento do Player
+    float speed = 20.0F;
+    //Velocidade de rotação do Player
+    float rotationSpeed = 120.0F;
+    //Aqui vai o prefab da bala
+    public GameObject bulletPrefab;
+    //Ponto de spawn da bala
+    public Transform bulletSpawn;
+
+    void Update() {
+        //Obtém o valor da entrada vertical e multiplica pela velocidade do Player
+        float translation = Input.GetAxis("Vertical") * speed;
+        //Obtém o valor da entrada horizontal e multiplica pela velocidade de rotação do Player
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //Ajusta a velocidade de movimento 
+        translation *= Time.deltaTime;
+        //Ajusta a velocidade de rotação
+        rotation *= Time.deltaTime;
+        //Move o Player para frente 
+        transform.Translate(0, 0, translation);
+        //Rotaciona o Player em torno do eixo Y 
+        transform.Rotate(0, rotation, 0);
+        //Verifica se a tecla de espaço foi pressionada
+        if(Input.GetKeyDown("space"))
+        {
+            //Instancia a bala a partir do prefab no ponto de spawn
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            //Aplica uma força para impulsionar a bala para a frente
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+    }
+}
---
+++ Assets/Scripts/ai1.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class ai1 : MonoBehaviour
+{
+    //Referência ao transform do Player
+    public Transform player;
+    //Ponto de spawn das balas
+    public Transform bulletSpawn;
+    //Barra de vida
+    public Slider healthBar;
+    //Prefab da bala
+    public GameObject bulletPrefab;
+    //Componente NavMeshAgent para controle de movimento
+    NavMeshAgent agent;
+    //Destino de movimento
+    public Vector3 destination;
+    //Posição alvo
+    public Vector3 target;
+    //Vida do Droid
+    float health = 100.0f;
+    //Velocidade de rotação
+    float rotSpeed = 5.0f;
+    //Alcance de visão
+    float visibleRange = 80.0f;
+    //Alcance do disparo
+    float shotRange = 40.0f;
+
+    void Start()
+    {
+        //Esta obtendo o componente NavMeshAgent 
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        //Chama repetidamente o método UpdateHealth
+        InvokeRepeating("UpdateHealth", 5, 0.5f);
+    }
+    void Update()
+    {
+        //Converte a posição do Droid para a posição na tela
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //Atualiza o valor da barra de vida 
+        healthBar.value = (int)health;
+        //posição da barra de vida em cima do Droid na tela
+        healthBar.transform.position = healthBarPos + new Vector3(0, 60, 0);
+    }
+    void UpdateHealth()
+    {
+        //Esta verificando se a vida atual está abaixo de 100, se estiver, ele almenta o valor da vida em 1
+        if (health < 100)
+            health++;
+    }
+    [Task]
+    public void PickRandomDestination()
+    {
+        //Gera uma posição de destino aleatória
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        //Define o destino do agente
+        agent.SetDestination(dest);
+        //Indica que a tarefa foi concluída 
+        Task.current.Succeed();
+    }
+    [Task]
+    public void MoveToDestination()
+    {
+        if (Task.isInspected)
+            //informações de depuração da tarefa
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        //Verifica se o agente chegou ao destino
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            //Indica que a tarefa foi concluída 
+            Task.current.Succeed();
+        }
+    }
+}
